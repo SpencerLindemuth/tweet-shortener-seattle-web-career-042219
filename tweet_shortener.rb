fc9af13ed578ba3569c00a6ad6d7db9a3734cfcb
Spencer Lindemuth <lindemut@gmail.com>
2019-03-29 17:50:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -51,7 +51,7 @@ def selective_tweet_shortener(tweet)
     return tweet
   end
 end
-    
+
 tww = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
 
 #binding.pry
